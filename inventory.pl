9780e0ca5671a6b5264159ee69486cfccddc3d0d
Ricky Yuliawan Pohan <rickyyuliawan21@gmail.com>
2020-11-23 02:24:55 +0700

    add inventory and items

---
+++ inventory.pl
@@ -0,0 +1,27 @@
+/* inventory.pl */
+
+:- dynamic(usedSpace/1).
+:- dynamic(stored/2).
+
+inventory(X) :- usedSpace(X).
+
+isInInventory(X) :- stored(X,Y), Y > 0.
+
+countItemInventory(Item, Count) :- stored(Item, Count).
+
+storeItem(X) :- (inventory(UsedSpace), UsedSpace < 100 ->
+                (stored(X,Y) -> retract(stored(X,Y)), Z is Y+1, 
+                asserta(stored(X,Z)), retract(usedSpace(UsedSpace)), 
+                NewUsedSpace is UsedSpace + 1, asserta(usedSpace(NewUsedSpace));
+                asserta(stored(X,1)), retract(usedSpace(UsedSpace)), 
+                NewUsedSpace is UsedSpace + 1, asserta(usedSpace(NewUsedSpace))),
+                write('Item telah ditambahkan ke inventory.'), nl;
+                write('Inventory Anda sudah penuh.'), nl).
+
+discardItem(Item, X) :- (isInInventory(Item) -> stored(Item, Y), Z is Y-X, 
+                        (Z =< 0 -> retract(stored(Item, Y)), retract(usedSpace(UsedSpace)),
+                        NewUsedSpace is UsedSpace - Y, asserta(usedSpace(NewUsedSpace));
+                        retract(stored(Item, Y)), asserta(stored(Item, Z)), retract(usedSpace(UsedSpace)),
+                        NewUsedSpace is UsedSpace - X, asserta(usedSpace(NewUsedSpace))),
+                        write('Item telah dibuang.'), nl;
+                        write('Anda tidak memiliki item yang mau dibuang.'), nl).
---
+++ items.pl
@@ -0,0 +1,64 @@
+/* items.pl */
+
+weapon(longsword, swordsman).
+weapon(shiningsword, swordsman).
+weapon(imperialsword, swordsman).
+weapon(excalibur, swordsman).
+weapon(ultimaweapon, swordsman).
+
+weapon(woodenbow, archer).
+weapon(platinumbow, archer).
+weapon(izanami, archer).
+weapon(omegabow, archer).
+weapon(savetheking, archer).
+
+weapon(magestaff, sorcerer).
+weapon(wizardstaff, sorcerer).
+weapon(darkstaff, sorcerer).
+weapon(fantasia, sorcerer).
+weapon(savethequeen, sorcerer).
+
+armor(robe, swordsman).
+armor(robe, archer).
+armor(robe, sorcerer).
+armor(brassarmor, swordsman).
+armor(brassarmor, archer).
+armor(brassarmor, sorcerer).
+armor(chainmail, swordsman).
+armor(chainmail, archer).
+armor(chainmail, sorcerer).
+armor(silverarmor, swordsman).
+armor(silverarmor, archer).
+armor(silverarmor, sorcerer).
+armor(dragonarmor, swordsman).
+armor(bloodarmor, archer).
+armor(dreamrobe, sorcerer).
+
+accessory(bandanna, swordsman).
+accessory(bandanna, archer).
+accessory(bandanna, sorcerer).
+accessory(cape, swordsman).
+accessory(cape, archer).
+accessory(cape, sorcerer).
+accessory(goldemblem, swordsman).
+accessory(goldemblem, archer).
+accessory(goldemblem, sorcerer).
+accessory(spiritbelt, swordsman).
+accessory(spiritbelt, archer).
+accessory(spiritbelt, sorcerer).
+accessory(fullhelmet, swordsman).
+accessory(windhat, archer).
+accessory(elementalhat, sorcerer).
+
+potion(medicine).
+potion(ether).
+potion(elixir).
+potion(highmedicine).
+potion(highether).
+potion(highelixir).
+potion(megamedicine).
+potion(megaether).
+potion(megaelixir).
+potion(xtramedicine).
+potion(xtraether).
+potion(xtraelixir).
