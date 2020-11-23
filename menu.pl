a57e361d64cef9fed00b58da1e3cb820e3d291b9
benidictusgalihmp <68523239+benidictusgalihmp@users.noreply.github.com>
2020-11-23 12:13:35 +0700

    added help.pl and menu.pl

---
+++ menu.pl
@@ -0,0 +1,32 @@
+main_Menu :-
+        write('                        [ Welcome to My World ]                     '), nl, nl,
+        write('####################################################################'), nl,
+        write('# 1. start       : untuk memulai petualanganmu                     #'), nl,
+        write('# 2. continue    : melanjutkan permainan terakhir                  #'), nl,
+        write('# 3. load        : memuat data permainan                           #'), nl,
+        write('# 4. help        : menampilkan segala bantuan                      #'), nl,
+        write('####################################################################'), nl.
+
+game_Menu :-
+        write('####################################################################'), nl,
+        write('# 1. map         : menampilkan peta                                #'), nl,
+        write('# 2. condition   : menampilkan kondisi pemain terkini              #'), nl,
+        write('# 3. stats       : menampilkan status pemain                       #'), nl,
+        write('# 4. rucksack    : menampilkan inventory pemain                    #'), nl,
+        write('# 5. shop        : mengunjungi shop                                #'), nl,
+        write('# 6. save        : menyimpan data permainan                        #'), nl,
+        write('# 7. load        : memuat data permainan                           #'), nl,
+        write('# 8. help        : menampilkan segala bantuan                      #'), nl,
+        write('####################################################################'), nl.
+
+shop_Menu :-
+        write('####################################################################'), nl,
+        write('# 1. gacha                      200  gold                          #'), nl,
+        write('# 2. potion                     50  gold                           #'), nl,
+        write('# 3. ether                      50  gold                           #'), nl,
+        write('# 4. antidote                   75  gold                           #'), nl,
+        write('# 5. anti paralyze              75 gold                            #'), nl,
+        write('# 6. x-Attack                   100 gold                           #'), nl,
+        write('# 7. x-Defense                  100 gold                           #'), nl,
+        write('# 8. exitShop                                                      #'), nl,
+        write('####################################################################'), nl.
