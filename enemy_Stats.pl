2102cb124c7e7bd7a2f7a40f160babda48923345
benidictusgalihmp <68523239+benidictusgalihmp@users.noreply.github.com>
2020-11-25 19:18:57 +0700

    change enemy_Stats.pl

--- enemy_Stats.pl
+++ enemy_Stats.pl
@@ -1,28 +1,73 @@
 /* fakta enemy */
-enemy(1, goblin).
-enemy(2, knight).
-enemy(3, slime).
-enemy(4, wolf).
-enemy(5, witch).
-enemy(6, ultimaDragon).
+:- dynamic(job/1).
+:- dynamic(att/1).
+:- dynamic(specialatt/2). % parameter 1 buat jumlah damage, parameter 2 buat cooldown
+:- dynamic(def/1).
+:- dynamic(currHP/1).
+:- dynamic(maxHP/1).
+:- dynamic(lvl/1).
 
-enemyHP(1, 50).
-enemyHP(2, 300).
-enemyHP(3, 30).
-enemyHP(4, 150).
-enemyHP(5, 750).
-enemyHP(6, 2000).
+% gold yang didapat setelah mengalahkan enemy
+gold(goblin, 200).
+gold(slime, 100).
+gold(wolf, 150).
+gold(knight, 500).
+gold(witch, 550).
+gold(ultimaDragon, 1000).
 
-enemyATK(1, 8).
-enemyATK(2, 20).
-enemyATK(3, 6).
-enemyATK(4, 50).
-enemyATK(5, 200).
-enemyATK(6, 500).
+% exp yang didapat setelah mengalahkan enemy
+exp(goblin, 40).
+exp(slime, 25).
+exp(wolf, 100).
+exp(knight, 500).
+exp(witch, 800).
+exp(ultimaDragon, 2000).
 
-enemyDEF(1, 8).
-enemyDEF(2, 100).
-enemyDEF(3, 10).
-enemyDEF(4, 40).
-enemyDEF(5, 80).
-enemyDEF(6, 200).
+% base stats enemy
+enemy(goblin) :-
+    asserta(att(8)), 
+    asserta(def(8)), 
+    asserta(currHP(50)), 
+    asserta(maxHP(50)), 
+    asserta(specialatt(20, 120)),
+    asserta(lvl(5)).
+
+enemy(slime) :-
+    asserta(att(6)), 
+    asserta(def(10)), 
+    asserta(currHP(30)), 
+    asserta(maxHP(30)),
+    asserta(specialatt(10, 25)),
+    asserta(lvl(5)).
+
+enemy(wolf) :-
+    asserta(att(50)), 
+    asserta(def(40)), 
+    asserta(currHP(150)), 
+    asserta(maxHP(150)), 
+    asserta(specialatt(300, 300)),
+    asserta(lvl(15)).
+
+enemy(knight) :-
+    asserta(att(20)), 
+    asserta(def(100)), 
+    asserta(currHP(300)), 
+    asserta(maxHP(300)), 
+    asserta(specialatt(200, 180)),
+    asserta(lvl(20)).
+
+enemy(witch) :-
+    asserta(att(200)), 
+    asserta(def(580)), 
+    asserta(currHP(750)), 
+    asserta(maxHP(750)), 
+    asserta(specialatt(75, 15)),
+    asserta(lvl(30)).
+
+enemy(ultimaDragon) :-
+    asserta(att(500)), 
+    asserta(def(200)), 
+    asserta(currHP(5000)), 
+    asserta(maxHP(5000)), 
+    asserta(specialatt(2500, 600)),
+    asserta(lvl(75)).
