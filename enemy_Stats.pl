e31b7f1e5024f3cc4f3c1518770b6ba35fecd5d6
benidictusgalihmp <68523239+benidictusgalihmp@users.noreply.github.com>
2020-11-25 18:58:25 +0700

    added enemy_stats.pl

---
+++ enemy_Stats.pl
@@ -0,0 +1,28 @@
+/* fakta enemy */
+enemy(1, goblin).
+enemy(2, knight).
+enemy(3, slime).
+enemy(4, wolf).
+enemy(5, witch).
+enemy(6, ultimaDragon).
+
+enemyHP(1, 50).
+enemyHP(2, 300).
+enemyHP(3, 30).
+enemyHP(4, 150).
+enemyHP(5, 750).
+enemyHP(6, 2000).
+
+enemyATK(1, 8).
+enemyATK(2, 20).
+enemyATK(3, 6).
+enemyATK(4, 50).
+enemyATK(5, 200).
+enemyATK(6, 500).
+
+enemyDEF(1, 8).
+enemyDEF(2, 100).
+enemyDEF(3, 10).
+enemyDEF(4, 40).
+enemyDEF(5, 80).
+enemyDEF(6, 200).
