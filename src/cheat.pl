a99976005af5c6037ac74bb63a5e275bd140820d
fakhrinail <fakhrinailwbw@gmail.com>
2020-11-27 21:23:56 +0700

    remove expr dari iamgod

--- src/cheat.pl
+++ src/cheat.pl
@@ -82,6 +82,4 @@ iamgod :-   retract(maxHP(MaxHP)), asserta(maxHP(9999)),
             retract(att(Attack)), asserta(att(9999)),
             retract(specialatt(SpAttack,_)), asserta(specialatt(9999,3)),
             retract(def(Defense)), asserta(def(9999)),
-            retract(expr(Exp)), asserta(expr(999999)),
-            levelUp,
             write('You are GOD.').
